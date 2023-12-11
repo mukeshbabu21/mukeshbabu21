5f2530b0a9a545ab694ae895b356d388c2020352
mukeshbabu21 <mukeshbabu2102@gmail.com>
2023-12-11 16:27:08 +0530

    save

--- src/main/java/com/jspiders/gitproject/MyFirstGitProject/App.java
+++ src/main/java/com/jspiders/gitproject/MyFirstGitProject/App.java
@@ -9,6 +9,7 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
-        System.out.println("hello");
+        System.out.println(1);
+        System.out.println(2);
     }
 }
